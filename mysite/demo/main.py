fb831933af2cab25e817fa375efae095d357d7b1
51201078 <51201078@hcmut.edu.vn>
2016-05-16 03:55:46 +0700

    ----------------------------- Update 1.2.3
    - Update Interface
    - Add some record
    - Add something new
    
    ----------------------------

--- mysite/demo/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from .models import Ticket
-
-class Main (object):
-    """Lõi xử lý chính"""
-
-    def get_by_date(date):
-        pass
--- mysite/demo/static/demo/js/main.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by admin on 29/04/2016.
- */
